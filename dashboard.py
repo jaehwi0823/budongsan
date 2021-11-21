7d070e6962daa4f307d0c8b8fe00168899ab934d
Jaehwi <iamhwii@gmail.com>
2021-11-21 15:41:14 +0900

    started streamlit

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import StrategyGround as SG
 import numpy as np
+import matplotlib.pyplot as plt
+import analysis_trend as at
 
 
 # 년월
@@ -125,13 +126,27 @@ def show_trend(indat, varn, lgd=None, price='거래금액', rolling=0):
         ax1.legend(lgd, loc='upper left')
     else:
         ax1.legend(vList, loc='upper left')
-    plt.show()
+    return fig
 
+# 데이터
+raw = pd.read_csv('./data/seoul_apt_buysell_2020.csv',
+                  encoding='CP949', low_memory=False)
+raw_v = get_ready(raw, type='bs')
 
+# 제목
+st.title("Hello, 부동산 실거래!")
 
-###############################################################################
-#  법정동 코드 정비
-###############################################################################
+# 사이드바
+data_type = st.sidebar.selectbox(
+    '관심 거래?',
+    ('매매','전세','월세')
+)
+region = st.sidebar.selectbox(
+    '관심 지역?',
+    ('강북','동서울','강동','강남','남서울','강서','서서울','도심','기타서울')
+)
+
+# 정보표출
 # 법정동 코드
 LAWD_CD = pd.read_csv('./data/법정동코드 전체자료.txt', sep='\t')
 LAWD_CD['level2'] = LAWD_CD['법정동명'].str.split(' ').apply(lambda x: x[0] + ' ' + x[1] if len(x) > 1 else x[0])
@@ -177,111 +192,45 @@ def get_gubun_cd(nm):
 def get_gubun_df(mydf, nm):
     return mydf[mydf.bjd_cd.isin(get_gubun_cd(nm))]
 
-get_lawd_cd('수서')
-get_lawd_cd_all('수서')
-get_gubun_cd('수서')
+def get_sub_regions(rg):
+    if rg == '강북':
+        return ['도봉구','강북구','성북구','노원구']
+    elif rg == '동서울':
+        return ['동대문구','중랑구','성동구','광진구']
+    elif rg == '강동':
+        return ['강동구','송파구']
+    elif rg == '강남':
+        return ['서초구','강남구']
+    elif rg == '남서울':
+        return ['동작구','관악구','금천구']
+    elif rg == '강서':
+        return ['강서구','양천구','영등포구','구로구']
+    elif rg == '서서울':
+        return ['은평구','마포구','서대문구']
+    elif rg == '도심':
+        return ['종로구','중구','용산구']
+    else:
+        return ['기타']
 
+# 표시
+st.write(region, " 지역은 ", get_sub_regions(region))
 
-
-###############################################################################
-#  전세
-###############################################################################
-# 서울 전세
-raw = pd.read_csv('./data/seoul_apt_junse_2020.csv',
-                  encoding='CP949', low_memory=False)
-raw_v = get_ready(raw, type='js')
-# raw_v.head()
-# raw_v.WEEKNUM.value_counts().sort_index().plot.bar(figsize=(20, 10))
-
-# 지역별
-tmp = raw_v[(raw_v.bjd_cd=='11500') & (raw_v.월세금액==0)]
-show_trend(tmp,
-           varn='bjd_cd',
-           lgd=['강서구'],
-           price='보증금액',
-           rolling=5)
-# 연령별
-show_trend(tmp, varn='old', 
-           lgd=['2016 이내','2011 이내','2006 이내','2001 이내','1996 이내','1991 이내','1986 이내'],
-           price='보증금액',
-           rolling=2)
-# 면적별
-show_trend(tmp, 
-           varn='area', 
-           lgd=['40m2 미만','60m2 미만','80m2 미만','90m2 미만','120m2 미만','120m2 이상'],
-           price='보증금액',
-           rolling=5)
-
-
-
-###############################################################################
-#  매매
-###############################################################################
-# 서울 매매
-raw = pd.read_csv('./data/seoul_apt_buysell_2020.csv',
-                  encoding='CP949', low_memory=False)
-raw_v = get_ready(raw, type='bs')
-
-
-# 지역별: '도심', '동서울', '강북', '서서울', '강서', '남서울', '강남', '강동'
-tmp = get_gubun_df(raw_v, '강서')
-show_trend(tmp,
+# 그래프
+tmp = get_gubun_df(raw_v, region)
+st.pyplot(show_trend(tmp,
            varn='bjd_cd',
            lgd=list(map(lambda x: SEOUL_DICT[x], np.sort(tmp.bjd_cd.unique()))),
-           rolling=5)
-# 연령별
-show_trend(tmp, 
-           varn='old', 
-           lgd=['2016 이내','2011 이내','2006 이내','2001 이내','1996 이내','1991 이내','1986 이내'],
-           rolling=5)
-# 면적별
-show_trend(tmp, 
-           varn='area', 
-           lgd=['40m2 미만','60m2 미만','80m2 미만','90m2 미만','120m2 미만','120m2 이상'],
-           rolling=5)
+           rolling=5))
 
 
+# example
+# left_column, right_column = st.columns(2)
+# # You can use a column just like st.sidebar:
+# left_column.button('Press me!')
 
-# 법정동코드 찾기
-interesting_name = '수서'
-get_lawd_cd_all(interesting_name)
-# 법정동코드 별
-tmp = raw_v[raw_v.bjd_cd=='11680']
-show_trend(tmp,
-           varn='bjd_cd',
-           lgd=[interesting_name],
-           rolling=5)
-# 연령별
-show_trend(tmp, 
-           varn='old', 
-           lgd=['2016 이내','2011 이내','2006 이내','2001 이내','1996 이내','1991 이내','1986 이내'],
-           rolling=5)
-# 면적별
-show_trend(tmp, 
-           varn='area', 
-           lgd=['40m2 미만','60m2 미만','80m2 미만','90m2 미만','120m2 미만','120m2 이상'],
-           rolling=5)
-
-
-
-
-myPrc = '거래금액'
-myVar = 'old'
-grp1 = tmp.groupby(['WEEKNUM',myVar])[['전용면적',myPrc]].sum()
-grp1['prc_area'] = grp1[myPrc] / grp1['전용면적']
-grp2 = tmp.groupby(['WEEKNUM']).size()
-
-# 그래프 준비
-plt.figure(figsize=(40, 15))
-plt.rc('font', family='AppleGothic')
-fig, ax1 = plt.subplots(figsize=(40, 15))
-ax2 = ax1.twinx()
-
-vList = np.sort(raw_v[myVar].unique())
-for cd in vList:
-    subgrp = grp1.xs(cd, level=1).reset_index()
-    ax1.plot(subgrp.WEEKNUM, subgrp['prc_area'])
-ax2.bar(grp1.reset_index()['WEEKNUM'].drop_duplicates(), grp2, alpha=0.2)
-plt.show()
-
-
+# # Or even better, call Streamlit functions inside a "with" block:
+# with right_column:
+#     chosen = st.radio(
+#         'Sorting hat',
+#         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+#     st.write(f"You are in {chosen} house!")
